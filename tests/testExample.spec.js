a07ebd5bddfaed5df8b188e8847f80c6527951b2
Sonalisha Patro <sona>
2025-11-16 18:22:05 +0530

    MyChanges

--- tests/testExample.spec.js
+++ tests/testExample.spec.js
@@ -3,5 +3,5 @@ test('Verify Google Title',async({page})=>{
     await page.goto('https://www.google.com/');
     expect(page.title).toBe('Google');
     expect(page).toHaveTitle('Google');
-    console.log("Opened Page URL",page.url);
+    console.log("Opened Page URL that showing",page.url);
 })
