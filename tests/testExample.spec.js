3183c2d74d14befddd2360ff6cc5d1100cefc31d
Sonalisha Patro <sona>
2025-11-16 13:44:29 +0530

    Jenkin Integration Of Playwright Learning

---
+++ tests/testExample.spec.js
@@ -0,0 +1,7 @@
+import { expect ,test } from "@playwright/test";
+test('Verify Google Title',async({page})=>{
+    await page.goto('https://www.google.com/');
+    expect(page.title).toBe('Google');
+    expect(page).toHaveTitle('Google');
+    console.log("Opened Page URL",page.url);
+})
